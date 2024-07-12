b56ad1c46c005516cd1c7681bc038b2317711127
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-07-13 00:09:12 +0200

    ajout Footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css"
 import Nav from "../Header/Nav";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Nav/>
+            <Footer/>
         </div>
     )
 }
