5a85e745e1c4ebbe79b085e57a5ea7b9f25a0278
Kiliana Blondron <kiliana.bldr@gmailcom>
2024-07-15 12:48:32 +0200

    Ajout de la Page Services

--- src/pages/Realisations/Realisations.jsx
+++ src/pages/Realisations/Realisations.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Realisations.css"
 import Nav from "../Header/Nav";
+import Footer from "../Footer/Footer";
 
 const Realisations = () =>{
     return (
         <div>
             <Nav/>
-
+            <Footer/>
         </div>
     )
 }
